49f2e65f8ffa0c3f2d1313c5595e8ff28e7c1361
Scottl4784 <Scottl4784@gmail.com>
2018-05-29 17:43:46 -0400

    refactored routes and added views

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const app = express()
-let hbs = require('hbs')
+const hbs = require('hbs')
 
 app.set("view engine", "hbs")
 
-app.get ('/', (req, res) => {
-    res.send('Welcome to Pizza Express')
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        data: 'Welcome to Pizza Express'
+    })
 })
-app.get ('/topping/:type', (req, res) => {
-    res.send(`${req.params.type} pizza! Good choice.`)
+app.get('/topping/:type', (req, res) => {
+    res.render('toppings', {
+        data: `${req.params.type} pizza! Good choice.`
+    })
 })
-app.get ('/order/:amount/:size', (req, res) => {
-    res.send(`Your order for ${req.params.amount} ${req.params.size} pizzaswill be ready in 1 minute.`)
+app.get('/order/:amount/:size', (req, res) => {
+    res.render('order', {
+        data: `Your order for ${req.params.amount} ${req.params.size} pizzaswill be ready in 1 minute.`
+    })
 })
 // assigning 3000 as our port
-const PORT    =  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // tells the server to listen for requests on port 3000
-app.listen(PORT, function(){
-  console.log("==========================")
-  console.log(`LISTENING ON PORT ${PORT}`);
-  console.log("==========================")
+app.listen(PORT, function () {
+    console.log("==========================")
+    console.log(`LISTENING ON PORT ${PORT}`);
+    console.log("==========================")
 });
