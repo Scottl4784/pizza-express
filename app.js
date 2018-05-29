47ef2d1dd54c38cc1cb9a16b39f10501aa056727
Scottl4784 <Scottl4784@gmail.com>
2018-05-29 17:27:43 -0400

    completed GET routes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 //require express package
 const express = require('express');
 //save an express module as 'app'
-const app     = express();
+const app = express();
+
+app.get ('/', (req, res) => {
+    res.send('Welcome to Pizza Express')
+})
+app.get ('/topping/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`)
+})
+app.get ('/order/:amount/:size', (req, res) => {
+    res.send(`Your order for ${req.params.amount} ${req.params.size} pizzaswill be ready in 1 minute.`)
+})
 // assigning 3000 as our port
 const PORT    =  process.env.PORT || 3000;
 
